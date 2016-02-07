86b2301cf1a3f07296ac359dce5515e2f6c78f73
Manoj Nama <manoj.nama@outlook.com>
2016-02-07 21:26:45 +0530

    Updated gzip compression settings

--- server/express.js
+++ server/express.js
@@ -8,13 +8,12 @@ var express = require('express'),
 	path = require('path');
 
 module.exports = function (app) {
-	app.use(compression());
    app.use(bodyParser.urlencoded({
       extended: true
    }));
    app.use(bodyParser.json());
 	app.use(cookieParser());
-
+   app.use(compression({threshold: 0}));
 	app.use(express.static(path.join(process.cwd(), 'client')));
 	app.set('appPath', 'client');
    app.set('absPath', path.join(process.cwd(), "client"));
