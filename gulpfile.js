7234ab9423b90eb91690deba757be83e72c5974b
Manoj Nama <manoj.nama@outlook.com>
2016-02-13 13:42:03 +0530

    Updated gulpfile to include font-awesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,40 @@
 'use strict';
 
 var gulp = require("gulp"),
-	inject = require("gulp-inject"),
-	concat = require("gulp-concat"),
+   inject = require("gulp-inject"),
+   concat = require("gulp-concat"),
    spawn = require('child_process').spawn,
-	bowerInject = require("main-bower-files"),
+   bowerInject = require("main-bower-files"),
    node;
 
 
-gulp.task("inject", function() {
-	console.log("Injecting into home.html");
-	var target = gulp.src("./client/home.html"),
-		sources = gulp.src(["./client/components/**/*.js", "./client/app/**/*.js", "./client/assets/styles/**/*.css"], {read: false}),
-		options = { relative: true };
+gulp.task("inject", function () {
+   console.log("Injecting into home.html");
+   var target = gulp.src("./client/home.html"),
+      sources = gulp.src(["./client/components/**/*.js", "./client/app/**/*.js", "./client/assets/styles/**/*.css"], {read: false}),
+      options = {relative: true};
 
-	return target.pipe(inject(gulp.src(bowerInject(), {read: false}), {name: "bower", relative: true}))
-		.pipe(inject(sources, options))
-		.pipe(gulp.dest("./client"));
+   return target.pipe(inject(gulp.src(bowerInject({
+      overrides: {
+         "font-awesome": {
+            main: ["./css/font-awesome.css"]
+         }
+      }
+   }), {read: false}), {name: "bower", relative: true}))
+      .pipe(inject(sources, options))
+      .pipe(gulp.dest("./client"));
 });
 
-gulp.task('scripts', function() {
-	return gulp.src(["./client/components/**/*.js", "./client/app/**/*.js"])
-		.pipe(concat("application.js"))
-		.pipe(gulp.dest("./.build/"));
+gulp.task('scripts', function () {
+   return gulp.src(["./client/components/**/*.js", "./client/app/**/*.js"])
+      .pipe(concat("application.js"))
+      .pipe(gulp.dest("./.build/"));
 });
 
-gulp.task('styles', function() {
-	return gulp.src(["./client/assets/**/*.css"])
-		.pipe(concat("application.css"))
-		.pipe(gulp.dest("./.build/"));
+gulp.task('styles', function () {
+   return gulp.src(["./client/assets/**/*.css"])
+      .pipe(concat("application.css"))
+      .pipe(gulp.dest("./.build/"));
 });
 
 gulp.task("serve", function () {
@@ -48,15 +54,15 @@ gulp.task("serve", function () {
       "./*.json",
       "./server/**/*.js",
       "./client/assets/styles/**/*.css"
-   ], function() {
+   ], function () {
       gulp.run('serve');
    })
 });
 
-process.on('exit', function() {
+process.on('exit', function () {
    if (node) node.kill();
 });
 
-gulp.task("build", ["scripts", "styles", "inject"], function() {
-	console.log("Building for prod..");
+gulp.task("build", ["scripts", "styles", "inject"], function () {
+   console.log("Building for prod..");
 });
