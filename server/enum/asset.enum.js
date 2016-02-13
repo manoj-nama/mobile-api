8bf8fbcf89de815b31feafc422084a15247faaa1
Manoj Nama <manoj.nama@outlook.com>
2016-02-13 23:43:59 +0530

    Added enums to manage assets type and extensions

---
+++ server/enum/asset.enum.js
@@ -0,0 +1,26 @@
+'use strict';
+
+exports.types = {
+   PDF: "PDF",
+   PRESENTATION: "PRESENTATION",
+   URL: "URL",
+   DOCUMENT: "DOCUMENT",
+   ARCHIVE: "ARCHIVE"
+};
+
+exports.typeList = [
+   "PDF",
+   "PRESENTATION",
+   "URL",
+   "DOCUMENT",
+   "ARCHIVE"
+];
+
+exports.extensions = {
+   PDF: "pdf",
+   PRESENTATION: ["ppt", "pptx", "odf"],
+   URL: "url",
+   DOCUMENT: ["doc", "docx"],
+   ARCHIVE: ["zip", "tar", "tar.gz"]
+};
+
