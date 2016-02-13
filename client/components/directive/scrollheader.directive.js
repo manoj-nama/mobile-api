ded34974a355e569ddfc1e30a7a34df4f11a0521
Manoj Nama <manoj.nama@outlook.com>
2016-02-13 13:43:22 +0530

    Updated the scroll header threshold value

--- client/components/directive/scrollheader.directive.js
+++ client/components/directive/scrollheader.directive.js
@@ -9,13 +9,12 @@
             scope: {},
             link: function (scope, elem, attrs) {
                var $window = $(window),
-                  scrollOffset = attrs.scrollOffset || 50,
+                  scrollOffset = attrs.scrollOffset || 30,
                   timer;
 
                $window.on("scroll", function() {
                   window.clearTimeout(timer);
                   timer = setTimeout(function () {
-                     console.log(">>>>", $window.scrollTop());
                      if($window.scrollTop() > scrollOffset) {
                         $(elem).addClass("scrolled");
                      } else {
